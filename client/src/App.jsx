7525b08c3788d72b2296e21c090c88dc36170328
bammytoye <bamigbalatoyese@gmail.com>
2024-11-05 12:52:08 +0100

    Header Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,27 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+// pages
+import HomePage from "./pages/HomePage"
+import About from "./pages/About"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import Profile from "./pages/Profile"
+
+// components
+import Header from "./components/Header"
 
 function App() {
 
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom"
+
+function Header() {
+    return (
+        <div className="bg-black text-white">
+            <div className="flex px-32 py-3 justify-between items-center cursor-pointer">
+                <div className="font-bold text-center">
+                    <Link to="/">
+                        <h1>MERN AUTH</h1>
+                        <span>APP</span>
+                    </Link>
+                </div>
+
+                <div>
+                    <ul className="flex space-x-5 text-lg">
+                        <Link to='/'>
+                            <li className="hover:text-gray-400 border-b-2 border-transparent hover:border-gray-400 transition duration-200">Home</li>
+                        </Link>
+                        <Link to='/about'>
+                            <li className="hover:text-gray-400 border-b-2 border-transparent hover:border-gray-400 transition duration-200">About</li>
+                        </Link>
+                        <Link to='/sign-in'>
+                            <li className="hover:text-gray-400 border-b-2 border-transparent hover:border-gray-400 transition duration-200">Sign In</li>
+                        </Link>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
