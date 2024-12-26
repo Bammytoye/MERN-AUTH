0b3a461adfc4f49be1691f089688a44ec038fd47
bammytoye <bamigbalatoyese@gmail.com>
2024-12-26 15:52:42 +0100

    redux-persist

--- client/src/redux/Store.jsx
+++ client/src/redux/Store.jsx
@@ -1,9 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from './User/UserSlice';
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({user: userReducer})
+
+const persistConfig = {
+    key: 'MernAuth',
+    version: 1,
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const Store = configureStore({
-    reducer: {user: userReducer},
+    reducer: persistedReducer,
     middleware:(getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false, // disable this if you are using redux-persist
     }) 
 })
+
+export const persistor = persistStore(Store)
